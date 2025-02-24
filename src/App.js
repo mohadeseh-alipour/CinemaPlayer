e39d6683a3be8175833ecfa76aa7d04cea5076e6
mohadeseh <mohadeseh.alipour27@gmail.com>
2025-02-24 16:44:50 +0100

    Feature: Clean up the title and data fetch

--- src/App.js
+++ src/App.js
@@ -31,20 +31,26 @@ export default function App() {
   }
 
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchMovies() {
       try {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
         );
+
         if (!res.ok)
           throw new Error("Something went wrong with fetching movie");
         const data = await res.json();
         if (data.Response === "False") throw new Error("Movie not found");
         setMovies(data.Search);
+        setError("");
       } catch (err) {
-        setError(err.message);
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
       } finally {
         setIsLoading(false);
       }
@@ -233,6 +239,9 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
   useEffect(() => {
     if (!title) return;
     document.title = `movie | ${title}`;
+    return function () {
+      document.title = "CinemaPlayer";
+    };
   }, [title]);
 
   return (
